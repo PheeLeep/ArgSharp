be0db04a7081d158474db43560235760a5799894
PheeLeep <ggprogrammin@gmail.com>
2026-03-08 12:23:55 +0800

    Add Default Value on ArgSharpCmd

--- ArgSharpCmd/Program.cs
+++ ArgSharpCmd/Program.cs
@@ -12,7 +12,7 @@ namespace ArgSharpCmd
             {
                 ArgSharpClass.Init("ArgSharpCmd", "ArgSharp Command Test", "Description 1", "Epilogue");
                 ArgSharpClass.AddArgument<string>(["-path"], "Path", "A file path.");
-                ArgSharpClass.AddArgument(["-sw", "--switch"], helpMsg: "A switch", defaultValue: false);
+                ArgSharpClass.AddArgument(["-sw", "--switch"], helpMsg: "A switch", defaultValue: false, isRequired: true);
                 ArgSharpClass.AddExample("ArgSharpCmd -sw");
                 if (!ArgSharpClass.Parse(args)) return;
 
